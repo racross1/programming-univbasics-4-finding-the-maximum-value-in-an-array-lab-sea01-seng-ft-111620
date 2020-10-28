d2da43253376b48c687b1e77cbe114974dc4d94f
Rachel Cross <racross1@gmail.com>
2020-10-28 14:24:36 +0000

    Automatically backed up by Learn

--- lib/finding_the_max_value.rb
+++ lib/finding_the_max_value.rb
@@ -1,12 +1,14 @@
+#def find_max_value(array)
+ #new_arr = array.sort 
+ #return new_arr[-1]
+#end 
+ 
+ 
 def find_max_value(array)
- new_arr = array.sort 
- return new_arr[-1]
-end 
- 
- 
-def find_max_value2(array)
   count = 0
+  max = nil
   while count < array.length do
+    if array[count] > array[count-1]
  
     count += 1
   end
